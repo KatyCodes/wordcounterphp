8a925b8c3e3e392e3baea7543726f399fd992911
Tim <tim.bourgault@gmail.com>
2016-10-04 14:52:33 -0700

    corrects error

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -30,34 +30,33 @@
       $this->assertEquals(5, $result);
     }
 
-  //
-  // function test_wordInStringTest()
-  //   {
-  //     //arrange
-  //     $test_RepeatCounter = new RepeatCounter;
-  //     $first_input = "HellO";
-  //     $second_input = "hello world hello hello hello";
-  //
-  //     //act
-  //     $result = $test_RepeatCounter->CountRepeats($first_input, $second_input);
-  //
-  //     $this->assertEquals("$first_input", $result);
-  //   }
-  //
-  //
-  // function test_wordInStringTestCase()
-  //   {
-  //     //arrange
-  //     $test_RepeatCounter = new RepeatCounter;
-  //     $first_input = "Hello";
-  //     $second_input = "HellO world helLo hello hello";
-  //
-  //     //act
-  //     $result = $test_RepeatCounter->CountRepeats($first_input, $second_input);
-  //
-  //     var_dump($result);
-  //     $this->assertEquals(4, $result);
-  //   }
+  function test_wordInStringTest()
+    {
+      //arrange
+      $test_RepeatCounter = new RepeatCounter;
+      $sentence = "hello world hello hello hello hello";
+      $word = "Hello";
+
+      //act
+      $result = $test_RepeatCounter->CountRepeats( $sentence, $word);
+
+      $this->assertEquals(5, $result);
+    }
+
+
+ function test_wordInStringTestCase() {
+
+   //arrange
+    $test_RepeatCounter = new RepeatCounter;
+    $sentence = "hello world hello hello hello hello";
+    $word = "Hello";
+
+    //act
+    $result = $test_RepeatCounter->CountRepeats( $sentence, $word);
+
+    //assert
+    $this->assertEquals(5, $result);
+   }
 
   }
 
